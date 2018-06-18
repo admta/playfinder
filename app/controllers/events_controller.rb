948aa575fa6d41e7f3fc6cd166c869cc0b947b2e
Mandy T <adamantia.t@gmail.com>
2018-06-18 18:10:29 +0200

    map search controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,32 @@
 class EventsController < ApplicationController
+
+  def index
+    if params[:query].present?
+      @events = Event.full_search(params[:query])
+    else
+      @events = Event.all
+    end
+    @markers = @events.where.not(latitude: nil, longitude: nil).map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        infoWindow: { content: render_to_string(partial: "../views/events/map_box.html.erb", locals: { place: place }) }
+      }
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+  def set_events
+    @event = Event.find(params[:event_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_time, :start_date, :end_time)
+  end
 end
+
+
