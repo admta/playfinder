50e364ec842397f68ed262a883f01f57481fed3c
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-19 12:45:05 +0200

    bucket list controller and list_event controller + bucket list show created

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,3 +1,5 @@
 class BucketList < ApplicationRecord
   belongs_to :user
+  has_many :list_events
+  has_many :list_places
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create :create_bucket_list
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :events, through: :bucket_list
   has_many :places, through: :bucket_list
   has_one :bucket_list
+
+  def create_bucket_list
+    @bucket_list = BucketList.new
+    @bucket_list.user = self
+    @bucket_list.save
+  end
 end
