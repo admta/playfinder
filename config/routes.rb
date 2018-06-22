591993304edf8656451f70187317be96cf4a9d24
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-22 16:21:36 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :bucketlists, only: [:new, :create, :show]
   resources :list_events, only: [:new, :create, :destroy]
   resources :list_places, only: [:new, :create, :destroy]
-  get '/show', to: 'places#show', to: 'events#show'
+  # get '/show', to: 'places#show', to: 'events#show'
 
 end
 
