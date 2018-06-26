05ba26a8eb3020ea805c557903383a79fb2dac2d
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-26 16:18:58 +0200

    worked on show page elements

--- app/javascript/components/changeBucketIcon.js
+++ app/javascript/components/changeBucketIcon.js
@@ -1,1 +1,13 @@
-document.querySelector(".icon-bucket")
+//
+// function changeBucketIcon () {
+//     document.querySelectorAll(".bucket-link").forEach((element) => {
+//
+//         element.addEventListener('click', (event) => {
+//             event.target.classList.add('bucket-disappear');
+//         });
+//     });
+// }
+//
+// export { changeBucketIcon };
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 import { smoothScrollingLinks } from '../components/smoothScrollingLinks';
 import { addGrayscaleToIcons} from "../components/addGrayscaleToIcons";
+// import { changeBucketIcon} from "../components/changeBucketIcon";
 import "../plugins/flatpickr"
 
 smoothScrollingLinks();
 addGrayscaleToIcons();
+// changeBucketIcon();
 
 
