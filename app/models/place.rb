b8f5f2d9aae7344ee01aea3a18c5aeaecdc6a441
Mandy T <adamantia.t@gmail.com>
2018-06-28 20:24:45 +0200

    extra filters

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,7 +7,7 @@ class Place < ApplicationRecord
   scope :age_0_2, -> { where("min_age = 0 AND max_age < 11" ) }
   scope :age_3_5, -> { where("min_age = 3 AND max_age < 16" ) }
   scope :age_9_15, -> { where("min_age = 9 AND max_age < 16" ) }
-
+  scope :age_0_5, -> { where("min_age >= 0 AND max_age < 6" ) }
 
 
   geocoded_by :address
