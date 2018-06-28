fa8c95cb5cbcb2416749030c0f38b73c8d037f19
Mandy T <adamantia.t@gmail.com>
2018-06-28 20:47:05 +0200

    all ages filter off

--- app/javascript/components/filterclickbutton.js
+++ app/javascript/components/filterclickbutton.js
@@ -1,8 +1,22 @@
 function filterClickButton() {
-  // Click on the checkbox
-  $("input[type=checkbox],#datepick").on('change', function(){
-      $("#search_form").submit();
-    });
+
+  if($('#age_0_2').is(':checked') || $('#age_3_5').is(':checked')) {
+    $("#all_ages").prop('checked', false);
+  }
+
+  if($('#all_ages').is(':checked')) {
+    $("#age_0_2,#age_3_5").prop('checked', false);
+  }
+
+  $('#all_ages').on('change',function() {
+    $("#age_0_2,#age_3_5").prop('checked', false);
+  });
+
+  // Auto submit
+  var $search_form = $("#search_form");
+  $search_form.find("input[type=checkbox],#datepick").on('change', function(){
+    $search_form.submit();
+  });
 }
 
 export { filterClickButton };
